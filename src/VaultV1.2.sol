fa8a2cb5f84726ac7c9ed0d98edff5e3c714a507
Eduardo <eduardonicoleit@hotmail.com>
2024-04-03 06:45:42 -0700

    fix:UsdtToken interface.

--- src/VaultV1.2.sol
+++ src/VaultV1.2.sol
@@ -44,7 +44,9 @@ interface UsdtToken {
     function transfer(address recipient, uint256 amount)
         external
         returns (bool);
-
+    function transferFrom(address sender, address recipient, uint256 amount)
+        external
+        returns (bool);
     function balanceOf(address account) external view returns (uint256);
 }
 
